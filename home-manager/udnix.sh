7d9ec338b21d2ae22177d6ad1ad36c4b126ae7ab
WillT253 <williamtreweeks@gmail.com>
2026-05-26 16:38:08 +0100

    Edited home-manager/udnix.sh to ensure correct hardware config is used

--- home-manager/udnix.sh
+++ home-manager/udnix.sh
@@ -6,9 +6,10 @@ export workingDir=$(pwd)
 
 cd $dotsDir
 
-git add -A
-$dotsDir/update-certs.sh
+git add -A # Ensure all files are at least staged in order for the rebuild command to recognise them
+$dotsDir/update-certs.sh # Update the certificate store
+sudo cp /etc/nixos/hardware-configuration.nix . # Ensure the correct hardware config is used by copying it from its original path on the system
 
-sudo nixos-rebuild switch --flake path:.
+sudo nixos-rebuild switch --flake . # The main event
 
 cd $workingDir
