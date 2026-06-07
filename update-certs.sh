ead4da764413fea45216df19d931a3e4919e2591
WillT253 <williamtreweeks@gmail.com>
2026-06-07 12:29:05 +0100

    Changed logic to allow for easier cert installation

--- update-certs.sh
+++ update-certs.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
-# Generates a Nix file containing an absolute path list of your untracked certificates
+# Generates a Nix file containing a path list of certificates
 CERT_DIR="$HOME/nixos-dotfiles/certs"
-
+PERMANENT_CERT_DIR="$HOME/certs"
 WORKING_DIR=$(pwd)
 
 cd ~/nixos-dotfiles/
 
 shopt -s nullglob
 
+rm certs/*
+
+for cert in "$PERMANENT_CERT_DIR"/*.pem "$PERMANENT_CERT_DIR"/*.crt; do
+  cp $cert "$CERT_DIR"/
+done
+
 echo "[" > cert-list.nix
 for cert in "$CERT_DIR"/*.pem; do
+  echo "  ./certs/$(basename "$cert")" >> cert-list.nix
+done
+for cert in "$CERT_DIR"/*.crt; do
   echo "  ./certs/$(basename "$cert")" >> cert-list.nix
 done
 echo "]" >> cert-list.nix
